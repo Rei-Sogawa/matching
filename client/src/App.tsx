edbf0a543cf79de5dee3e30be66e37fdb8ca9fa9
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-04-02 15:41:49 +0900

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,37 @@
-import { Button } from "antd-mobile";
+import { Swiper } from "antd-mobile";
+
+function Fruits() {
+  return (
+    <Swiper style={{ height: "100%" }} indicator={() => null}>
+      <Swiper.Item>
+        <div className="h-full bg-yellow-200">
+          <div className="h-full flex flex-col justify-center items-center">
+            <div className="font-bold">BANANA</div>
+          </div>
+        </div>
+      </Swiper.Item>
+      <Swiper.Item>
+        <div className="h-full bg-red-400">
+          <div className="h-full flex flex-col justify-center items-center">
+            <div className="font-bold">APPLE</div>
+          </div>
+        </div>
+      </Swiper.Item>
+      <Swiper.Item>
+        <div className="h-full bg-purple-200">
+          <div className="h-full flex flex-col justify-center items-center">
+            <div className="font-bold">GRAPE</div>
+          </div>
+        </div>
+      </Swiper.Item>
+    </Swiper>
+  );
+}
 
 export function App() {
   return (
-    <div className="h-screen bg-gray-50">
-      <div className="h-full flex flex-col items-center justify-center">
-        <Button>Fuck</Button>
-      </div>
+    <div className="h-full bg-gray-50">
+      <Fruits />
     </div>
   );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import babel from "vite-plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), babel()],
 });
