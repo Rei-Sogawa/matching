ac7f3526dfc7f1ddcd6b081ecd329ba33c860c02
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-04-03 14:51:46 +0900

    fix

---
+++ server/src/firebase-app.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import * as admin from "firebase-admin";
+
+if (process.env.NODE_ENV === "production") {
+  process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS_PATH;
+} else {
+  const PROJECT_ID = "playground-firebase-abc--test";
+  const FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099";
+  const FIRESTORE_EMULATOR_HOST = "localhost:8080";
+
+  process.env.GCLOUD_PROJECT = PROJECT_ID;
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = FIREBASE_AUTH_EMULATOR_HOST;
+  process.env.FIRESTORE_EMULATOR_HOST = FIRESTORE_EMULATOR_HOST;
+}
+
+export function getAdmin() {
+  if (admin.apps.length > 0) {
+    return admin.apps[0] as admin.app.App;
+  } else {
+    const app = admin.initializeApp();
+    return app;
+  }
+}
+export const getAuth = () => getAdmin().auth();
+export const getDb = () => getAdmin().firestore();
---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import "./firebase-app";
+
+import express from "express";
+import { getDb } from "./firebase-app";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const name = process.env.NAME || "World";
+  await getDb().collection("users").add({ createdAt: new Date().toString() });
+  res.send(`Hello Fucking ${name}!.`);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`helloworld: listening on port ${port}`);
+});
