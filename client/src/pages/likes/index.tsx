b9bd3f099d0a949420b524260d3a3625f1cce703
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-04-05 21:02:38 +0900

    fix

--- client/src/pages/likes/index.tsx
+++ client/src/pages/likes/index.tsx
@@ -27,7 +27,7 @@ const UserCard: FC<UserCardProps> = ({ index, onShow, onHide }) => {
 
 const LikeBadge: FC = () => {
   return (
-    <div className="absolute top-1/4 left-3/4 z-10 origin-top rotate-12 px-3 py-2 border-4 rounded border-green-500 text-green-500 font-bold text-4xl">
+    <div className="absolute top-1/4 right-1/4 z-10 origin-top rotate-12 px-3 py-2 border-4 rounded border-green-500 text-green-500 font-bold text-4xl">
       LIKE
     </div>
   );
