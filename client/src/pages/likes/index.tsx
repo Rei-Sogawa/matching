f61c8459e9b3ad13598d42ed78db1b58834344f6
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-04-05 20:57:30 +0900

    like nope

--- client/src/pages/likes/index.tsx
+++ client/src/pages/likes/index.tsx
@@ -3,25 +3,69 @@ import "swiper/css/effect-cards";
 import "swiper/css/navigation";
 import "./index.css";
 
-import { FC, useEffect } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 import { EffectCards, Virtual } from "swiper";
-import { Swiper, SwiperSlide, useSwiper, useSwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiperSlide } from "swiper/react";
 
-const UserCard: FC<{ index: number }> = ({ index }) => {
-  const swiper = useSwiper();
+type UserCardProps = {
+  index: number;
+  onShow: (index: number) => void;
+  onHide: (index: number) => void;
+};
+
+const UserCard: FC<UserCardProps> = ({ index, onShow, onHide }) => {
   const swiperSlide = useSwiperSlide();
   useEffect(() => {
-    console.log(index);
-    return () => {
-      console.log(index);
-    };
-  }, []);
+    if (swiperSlide.isVisible) {
+      onShow(index);
+    } else {
+      onHide(index);
+    }
+  }, [swiperSlide.isVisible]);
   return <div className="h-full flex justify-center items-center">{`Slide ${index}`}</div>;
 };
 
-export const Likes: FC = () => {
+const LikeBadge: FC = () => {
   return (
-    <div className="h-full bg-white">
+    <div className="absolute top-1/4 left-3/4 z-10 origin-top rotate-12 px-3 py-2 border-4 rounded border-green-500 text-green-500 font-bold text-4xl">
+      LIKE
+    </div>
+  );
+};
+
+const NopeBadge: FC = () => {
+  return (
+    <div className="absolute top-1/4 left-1/4 z-10 origin-top -rotate-12 px-3 py-2 border-4 rounded border-red-500 text-red-500 font-bold text-4xl">
+      NOPE
+    </div>
+  );
+};
+
+export const Likes: FC = () => {
+  const [visibleSlides, setVisibleSlides] = useState<number[]>([]);
+  const onShow = (index: number) => setVisibleSlides((prev) => [...new Set([...prev, index])]);
+  const onHide = (index: number) => setVisibleSlides((prev) => prev.filter((_index) => _index !== index));
+
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+  useEffect(() => {
+    if (visibleSlides.length === 1) setActiveSlide(visibleSlides[0]);
+  }, [visibleSlides]);
+
+  const toLike = useMemo(() => {
+    if (visibleSlides.length === 2) {
+      return visibleSlides.some((index) => index < activeSlide);
+    }
+    return false;
+  }, [visibleSlides, activeSlide]);
+  const toNope = useMemo(() => {
+    if (visibleSlides.length === 2) {
+      return visibleSlides.some((index) => index > activeSlide);
+    }
+    return false;
+  }, [visibleSlides, activeSlide]);
+
+  return (
+    <div className="h-full bg-white relative">
       <Swiper
         effect="cards"
         virtual
@@ -33,10 +77,12 @@ export const Likes: FC = () => {
       >
         {Array.from({ length: 100 }).map((_, index) => (
           <SwiperSlide key={index} virtualIndex={index} className="bg-gray-200">
-            <UserCard index={index} />
+            <UserCard index={index} onShow={onShow} onHide={onHide} />
           </SwiperSlide>
         ))}
       </Swiper>
+      {toLike && <LikeBadge />}
+      {toNope && <NopeBadge />}
     </div>
   );
 };
