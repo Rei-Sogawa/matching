9015ef213a2072398eed7826cca2634446d1b0b4
rtokkie <natto.and.eggs@gmail.com>
2022-04-24 15:38:33 +0900

    fix

--- server/src/resolvers/root-types/index.ts
+++ server/src/resolvers/root-types/index.ts
@@ -1,3 +1,5 @@
+import { addMinutes } from "date-fns";
+
 import { Resolvers } from "./../../graphql/generated";
 
 export const Me: Resolvers["Me"] = {};
@@ -5,6 +7,17 @@ export const Me: Resolvers["Me"] = {};
 export const User: Resolvers["User"] = {
   photoUrls: async (parent, _args, context) => {
     const { storage } = context;
-    return parent.photoPaths.map((path) => storage.bucket().file(path).publicUrl());
+
+    if (process.env.NODE_ENV !== "production") {
+      return parent.photoPaths.map((path) => storage.bucket().file(path).publicUrl());
+    }
+
+    return parent.photoPaths.map((path) =>
+      storage
+        .bucket()
+        .file(path)
+        .getSignedUrl({ action: "read", expires: addMinutes(new Date(), 15) })
+        .then((res) => res[0] as string)
+    );
   },
 };
