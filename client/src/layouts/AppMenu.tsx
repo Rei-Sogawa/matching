0f524f04028627138c39d4ceba4ed96d1171a915
rtokkie <natto.and.eggs@gmail.com>
2022-05-01 10:45:39 +0900

    fix

--- client/src/layouts/AppMenu.tsx
+++ client/src/layouts/AppMenu.tsx
@@ -1,15 +1,17 @@
 import { Box, Container, Flex, HStack } from "@chakra-ui/react";
 import { FC } from "react";
 import { BiLike, BiMessageRoundedDots, BiSearch, BiUser } from "react-icons/bi";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
+import { AppLink } from "../components/base/AppLink";
 import { routes } from "../routes";
 
 export const AppMenu: FC = () => {
   const location = useLocation();
-  const navigate = useNavigate();
 
   const rootPath = location.pathname.split("/")[1];
+
+  const isActive = (_rootPath: string) => _rootPath === rootPath;
 
   return (
     <>
@@ -27,51 +29,41 @@ export const AppMenu: FC = () => {
       >
         <Container h="full">
           <HStack h="full" justifyContent="space-between" alignItems="center">
-            <Flex
-              color={rootPath === "" ? "black" : "gray.500"}
-              direction="column"
-              alignItems="center"
-              cursor="pointer"
-            >
-              <BiSearch fontSize="28px" />
-              <Box fontWeight="bold" fontSize="xs">
-                さがす
-              </Box>
-            </Flex>
-            <Flex
-              color={rootPath === "" ? "black" : "gray.500"}
-              direction="column"
-              alignItems="center"
-              cursor="pointer"
-            >
-              <BiLike fontSize="28px" />
-              <Box fontWeight="bold" fontSize="xs">
-                いいね
-              </Box>
-            </Flex>
-            <Flex
-              color={rootPath === "" ? "black" : "gray.500"}
-              direction="column"
-              alignItems="center"
-              cursor="pointer"
-            >
-              <BiMessageRoundedDots fontSize="28px" />
-              <Box fontWeight="bold" fontSize="xs">
-                メッセージ
-              </Box>
-            </Flex>
-            <Flex
-              color={rootPath === "my-page" ? "black" : "gray.500"}
-              direction="column"
-              alignItems="center"
-              cursor="pointer"
-              onClick={() => navigate(routes["/my-page"].path())}
-            >
-              <BiUser fontSize="28px" />
-              <Box fontWeight="bold" fontSize="xs">
-                マイページ
-              </Box>
-            </Flex>
+            <AppLink to="#" color={isActive("") ? "black" : "gray.500"}>
+              <Flex direction="column" alignItems="center">
+                <BiSearch fontSize="28px" />
+                <Box fontWeight="bold" fontSize="xs">
+                  さがす
+                </Box>
+              </Flex>
+            </AppLink>
+
+            <AppLink to="#" color={isActive("") ? "black" : "gray.500"}>
+              <Flex direction="column" alignItems="center">
+                <BiLike fontSize="28px" />
+                <Box fontWeight="bold" fontSize="xs">
+                  いいね
+                </Box>
+              </Flex>
+            </AppLink>
+
+            <AppLink to="#" color={isActive("") ? "black" : "gray.500"}>
+              <Flex direction="column" alignItems="center">
+                <BiMessageRoundedDots fontSize="28px" />
+                <Box fontWeight="bold" fontSize="xs">
+                  メッセージ
+                </Box>
+              </Flex>
+            </AppLink>
+
+            <AppLink to={routes["/my-page"].path()} color={isActive("my-page") ? "black" : "gray.500"}>
+              <Flex direction="column" alignItems="center">
+                <BiUser fontSize="28px" />
+                <Box fontWeight="bold" fontSize="xs">
+                  マイページ
+                </Box>
+              </Flex>
+            </AppLink>
           </HStack>
         </Container>
       </Box>
