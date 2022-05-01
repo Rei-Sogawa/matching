8e0b6622a70891e8435ad66d959e46088b1838da
rtokkie <natto.and.eggs@gmail.com>
2022-05-01 11:22:40 +0900

    fix

--- server/src/utils/decode-file.ts
+++
@@ -1,8 +0,0 @@
-export const decodeFile = (encoded: string) => {
-  const fileBuffer = encoded.replace(/^data:\w+\/\w+;base64,/, "");
-  const contentType = encoded.slice(encoded.indexOf(":") + 1, encoded.indexOf(";"));
-
-  const buffer = Buffer.from(fileBuffer, "base64");
-
-  return { buffer, contentType };
-};
