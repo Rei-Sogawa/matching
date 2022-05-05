9e079c3b8a37c58ba8569db73c436ec7e7bda0ba
rtokkie <natto.and.eggs@gmail.com>
2022-05-05 12:59:28 +0900

    fix

--- server/src/fire/docs/like-index.ts
+++ server/src/fire/docs/like-index.ts
@@ -1,18 +1,38 @@
 import { FireDocument } from "@rei-sogawa/unfireorm";
+import { merge, omit } from "lodash";
 
 import { LikeData } from "./like";
 
 export type LikeIndexData = {
-  index: {
-    [key in string]: LikeData;
+  root: {
+    [id in string]: LikeData;
   };
 };
 
 export class LikeIndexDoc extends FireDocument<LikeIndexData> {
-  index!: LikeIndexData;
+  root!: {
+    [id in string]: LikeData;
+  };
 
   toData() {
-    const { index } = this;
-    return index;
+    const { id, ref, ...data } = this;
+    return data;
+  }
+
+  toBatch() {
+    const { id, ref, ...data } = this;
+    return [ref, data];
+  }
+
+  addIndex(id: string, data: LikeData) {
+    return this.edit({ root: merge(this.root, { [id]: data }) });
+  }
+
+  editIndex(id: string, data: LikeData) {
+    return this.edit({ root: merge(this.root, { [id]: data }) });
+  }
+
+  removeIndex(id: string) {
+    return this.edit({ root: omit(this.root, [id]) });
   }
 }
